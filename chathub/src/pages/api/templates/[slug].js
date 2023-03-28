8cd433592767300cbb4ef3670894e56070bb8501
Aakash N S <aakashns@jovian.ai>
2023-03-28 15:24:35 +0000

    remove streaming

--- chathub/src/pages/api/templates/[slug].js
+++ chathub/src/pages/api/templates/[slug].js
@@ -21,6 +21,12 @@ export default async function handler(req, res) {
   template.systemPrompt = systemPrompt;
   template.userPrompt = userPrompt;
 
+  const DEFAULT_OPTIONS = {
+    model: "gpt-3.5-turbo",
+    max_tokens: 800,
+    temperature: 0.8,
+  };
+
   if (req.method === "POST") {
     const { inputs, apiKey, options, messages = [] } = req.body;
     const fullMessages = [
@@ -28,8 +34,19 @@ export default async function handler(req, res) {
       { role: "user", content: fillTemplate(userPrompt, inputs) },
       ...messages,
     ];
-    const stream = await startOpenAIStream(fullMessages, apiKey, options);
-    return new Response(stream);
+
+    return await fetch("https://api.openai.com/v1/chat/completions", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${apiKey}`,
+      },
+      method: "POST",
+      body: JSON.stringify({
+        messages: fullMessages,
+        ...DEFAULT_OPTIONS,
+        ...options,
+      }),
+    });
   } else {
     res.status(200).json({ success: true, data: template });
   }

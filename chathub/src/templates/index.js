f394117483f96994ebf554b38bbcf35bbc72340c
Aakash N S <aakashns@jovian.ai>
2023-03-28 07:23:30 +0000

    Add YouTube title creator

--- chathub/src/templates/index.js
+++ chathub/src/templates/index.js
@@ -20,4 +20,11 @@ export default [
       "Breaks down any piece of code and provides a clear explanation of its functionality, helping you understand programming concepts and improve your coding skills. ",
     iconUrl: "https://i.imgur.com/gznl8L8.png",
   },
+  {
+    slug: "youtube-title-generator",
+    title: "YouTube Title Generator",
+    description:
+      "Brainstorm creative and attention-grabbing titles for your YouTube videos, to generate more clicks and views.",
+    iconUrl: "https://i.imgur.com/0BloXDE.png",
+  },
 ];

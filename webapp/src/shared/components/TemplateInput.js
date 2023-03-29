b1f758b4e42b8a1a77d0331fca364317fee095b8
Aakash N S <aakashns@jovian.ai>
2023-03-29 13:21:15 +0000

    fix template input for text

--- webapp/src/shared/components/TemplateInput.js
+++ webapp/src/shared/components/TemplateInput.js
@@ -54,6 +54,8 @@ export const TemplateInput = ({
               id={field}
               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               placeholder={placeholder}
+              value={value || ""}
+              onChange={(e) => onChange(e.target.value)}
             />
           </div>
         </div>

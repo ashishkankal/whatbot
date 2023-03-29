274170080b6c948cf5c3e78a0556295b4a8ad2ce
Aakash N S <aakashns@jovian.ai>
2023-03-29 06:08:48 +0000

    fix caching

--- webapp/src/shared/network/index.js
+++ webapp/src/shared/network/index.js
@@ -2,16 +2,31 @@ export const TEMPLATES_BASE_URL =
   "https://raw.githubusercontent.com/JovianHQ/chathub/main/templates";
 
 export async function getTemplates() {
-  const res = await fetch(`${TEMPLATES_BASE_URL}/index.json`);
+  const res = await fetch(
+    `${TEMPLATES_BASE_URL}/index.json?t=${new Date().getTime()}`,
+    {
+      cache: "no-store",
+    }
+  );
   return res.json();
 }
 
 export async function getSystemPrompt(slug) {
-  const res = await fetch(`${TEMPLATES_BASE_URL}/${slug}/system.md`);
+  const res = await fetch(
+    `${TEMPLATES_BASE_URL}/${slug}/system.md?t=${new Date().getTime()}`,
+    {
+      cache: "no-store",
+    }
+  );
   return res.text();
 }
 
 export async function getUserPrompt(slug) {
-  const res = await fetch(`${TEMPLATES_BASE_URL}/${slug}/user.md`);
+  const res = await fetch(
+    `${TEMPLATES_BASE_URL}/${slug}/user.md?t=${new Date().getTime()}`,
+    {
+      cache: "no-store",
+    }
+  );
   return res.text();
 }

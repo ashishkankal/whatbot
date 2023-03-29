7afc554b660753a300b28171b4877f7c849eb519
Aakash N S <aakashns@jovian.ai>
2023-03-29 13:29:55 +0000

    add google analytics

--- webapp/src/pages/_document.js
+++ webapp/src/pages/_document.js
@@ -1,10 +1,29 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-D22CC4EHE4`}
+        />
+        <Script
+          strategy="afterInteractive"
+          id="googleanalytics"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-G-D22CC4EHE4', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
       </Head>
       <body>
         <Main />

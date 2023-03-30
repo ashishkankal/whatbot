f8e9bfac3086b583ba61fed19cb3beabc11637bd
Aakash N S <aakashns@jovian.ai>
2023-03-30 11:42:39 +0000

    fix header

--- webapp/src/pages/api/templates/index.js
+++ webapp/src/pages/api/templates/index.js
@@ -3,7 +3,6 @@ import { getTemplates } from "@/shared/network";
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET,OPTIONS,PATCH,DELETE,POST,PUT"

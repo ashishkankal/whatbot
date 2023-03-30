0142deede80c6489ff7454fdc6eae8747590175b
Aakash N S <aakashns@jovian.ai>
2023-03-30 11:40:13 +0000

    allow cors

--- webapp/src/pages/api/templates/index.js
+++ webapp/src/pages/api/templates/index.js
@@ -1,6 +1,17 @@
 import { getTemplates } from "@/shared/network";
 
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
   const templates = await getTemplates();
   res.status(200).json({ success: true, data: templates });
 }
